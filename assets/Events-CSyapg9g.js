3b1f14f9cb6a65f2cc02d6027532919bf3575e88
egor0 <alloa1955@gmail.com>
2026-02-21 21:19:50 +0200

    ver 1.0.5

---
+++ assets/Events-CSyapg9g.js
@@ -0,0 +1,1 @@
+import{j as t,N as e}from"./index-B-0TKyn_.js";function n(){return t.jsx("div",{id:"events",children:t.jsx(e,{})})}export{n as default};
--- assets/Events-DsROlWBG.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as e}from"./index-CgtAmPex.js";function n(){return t.jsx("div",{id:"events",children:t.jsx(e,{})})}export{n as default};
