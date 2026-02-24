33a7748b0ee8fca82de3f3bce5da214147aa651b
egor0 <alloa1955@gmail.com>
2026-02-24 20:21:36 +0200

    ver 1.1.0

--- assets/Gifts-Bu98AxA6.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as s}from"./index-BO2m7w7Z.js";function o(){return t.jsx("div",{id:"gifts",children:t.jsx(s,{})})}export{o as default};
---
+++ assets/Gifts-ECFcybCV.js
@@ -0,0 +1,1 @@
+import{j as t,N as s}from"./index-Bq15_sYO.js";function o(){return t.jsx("div",{id:"gifts",children:t.jsx(s,{})})}export{o as default};
