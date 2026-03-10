b872287402d37e6e89d30e7dff07190992a6efb3
egor0 <alloa1955@gmail.com>
2026-03-10 23:33:55 +0200

    ver 1.2.4

--- assets/GiftsComponent-C_qW_ePY.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as s}from"./index-CQpd0C7H.js";function o(){return t.jsx("div",{id:"gifts",children:t.jsx(s,{})})}export{o as default};
---
+++ assets/RulesComponents-Bu3ry20T.js
@@ -0,0 +1,1 @@
+import{j as s,N as r}from"./index-DY0Djp0p.js";function e(){return s.jsx("div",{id:"rules",children:s.jsx(r,{})})}export{e as default};
