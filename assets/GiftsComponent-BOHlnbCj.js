c3e5f24a583428697239ca25e2d48bdf6bded992
egor0 <alloa1955@gmail.com>
2026-03-27 13:02:02 +0200

    ver 1.3.2.7

--- assets/GiftsComponent-BOHlnbCj.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as s}from"./index-VhV8NqWU.js";function o(){return t.jsx("div",{id:"gifts",children:t.jsx(s,{})})}export{o as default};
---
+++ assets/GiftsComponent-C3saB2hS.js
@@ -0,0 +1,1 @@
+import{j as t,N as s}from"./index-D5xe7Kr2.js";function o(){return t.jsx("div",{id:"gifts",children:t.jsx(s,{})})}export{o as default};
